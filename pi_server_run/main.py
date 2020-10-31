4fe357901d3c9739adb4fc8c3d415970df5dbf84
bigflood <pyotaesu@gmail.com>
2020-10-31 15:03:48 +0900

    add image resolution param

--- pi_server_run/main.py
+++ pi_server_run/main.py
@@ -18,12 +18,23 @@ def hello_world():
 
 @app.route('/picam')
 def serve_picamera_image():
+    w = int(request.args.get('w') or 0)
+    h = int(request.args.get('h') or 0)
+    wh_ratio = 0.75
+    if w and not h:
+        h = int(w * wh_ratio)
+    elif h and not w:
+        w = int(h / wh_ratio)
+    if not w and not h:
+        w = 960
+        h = int(w * wh_ratio)
+
     jpeg_quality = int(request.args.get('q') or 50)
     fmt = 'jpeg'
     buf = io.BytesIO()
     # https://picamera.readthedocs.io/en/release-1.10/api_camera.html
     with PiCamera() as camera:
-        camera.resolution = (2592, 1944)
+        camera.resolution = (w, h)
         camera.rotation = 180
         camera.start_preview()
         sleep(2)
@@ -31,7 +42,7 @@ def serve_picamera_image():
         start_time = time()
         camera.capture(buf, fmt, quality=jpeg_quality)
         camera.stop_preview()
-    app.logger.info(f'capture: time={time()-start_time}, size={buf.tell()}, jpeg_quality={jpeg_quality}')
+    app.logger.info(f'capture: time={time()-start_time:.2f}s, size={w}x{h} ({buf.tell()} bytes), jpeg_quality={jpeg_quality}')
     buf.seek(0)
     return send_file(buf, f'image/{fmt}')
 
