25b506ccde6a8ec650daa9a06e1bceacc4f68246
bigflood <pyotaesu@gmail.com>
2020-10-31 09:14:30 +0900

    add serve_picamera_image

--- pi_server_run/main.py
+++ pi_server_run/main.py
@@ -1,6 +1,9 @@
 from typing import Optional, List
-from flask import Flask
+from flask import Flask, send_file
+from time import sleep
+import io
 import logging
+from picamera import PiCamera
 import pi_server
 
 __version__ = pi_server.__version__
@@ -11,6 +14,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return f'Hello World! {__version__}'
+
+
+@app.route('/picam')
+def serve_picamera_image():
+    fmt = 'jpeg'
+    buf = io.BytesIO()
+    with PiCamera() as camera:
+        camera.rotation = 180
+        camera.start_preview()
+        sleep(2)
+        camera.capture(buf, fmt)
+        camera.stop_preview()
+    buf.seek(0)
+    return send_file(buf, f'image/{fmt}')
 
 
 def main(args: Optional[List[str]] = None) -> int:
